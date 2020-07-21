4098b61769249617ea0bfadc2c49092010056e23
suragch <studymongolian@gmail.com>
2020-07-21 11:13:16 +0800

    initial draft

---
+++ 04-control-flow/projects/challenge/challenge.dart
@@ -0,0 +1,124 @@
+// Copyright (c) 2020 Razeware LLC
+// For full license & permission details, see LICENSE.
+
+void main() {
+  /// Challenge 1: Find the error
+  ///
+  /// What's wrong with the following code?
+  ///
+  /// ```
+  /// const firstName = 'Bob';
+  /// if (firstName == 'Bob') {
+  ///   const lastName = 'Smith';
+  /// } else if (firstName == 'Ray') {
+  ///   const lastName = 'Wenderlich';
+  /// }
+  /// final fullName = firstName + ' ' + lastName;
+  /// ```
+
+  // `lastName` was declared in the `if` statement scope but was used
+  // in the parent scope. You need to declare it at or above the scope
+  // where it is needed.
+  const firstName = 'Bob';
+  var lastName = '';
+  if (firstName == 'Bob') {
+    lastName = 'Smith';
+  } else if (firstName == 'Ray') {
+    lastName = 'Wenderlich';
+  }
+  final fullName = firstName + ' ' + lastName;
+  print(fullName);
+
+  /// Challenge 2: Boolean challenge
+  ///
+  /// In each of the following statements, what is the value of the
+  /// Boolean expression?
+  ///
+  /// ```
+  /// true && true
+  /// false || false
+  /// (true && 1 != 2) || (4 > 3 && 100 < 1)
+  /// ((10 / 2) > 3) && ((10 % 2) == 0)
+  /// ```
+
+  print(true && true); // true
+  print(false || false); // false
+  print((true && 1 != 2) || (4 > 3 && 100 < 1)); // true
+  print(((10 / 2) > 3) && ((10 % 2) == 0)); // true
+
+  /// Challenge 3: Next power of two
+  ///
+  /// Given a number, determine the next power of two above or equal to
+  /// that number.
+
+  const number = 946;
+  var trial = 1;
+  var times = 0;
+  while (trial < number) {
+    trial = trial * 2;
+    times += 1;
+  }
+  print('Next power of 2 >= $number is $trial '
+      'which is 2 to the power of $times.');
+  // Next power of 2 >= 946 is 1024 which is 2 to the power of 10.
+
+  /// Challenge 4: Fibonacci
+  ///
+  /// Calculate the _n_th Fibonacci number. Remember that Fibonacci numbers
+  /// start its sequence with 1 and 1, and then subsequent numbers in the
+  /// sequence are equal to the previous two values added together. You can
+  /// get a refresher here: https://en.wikipedia.org/wiki/Fibonacci_number
+
+  const n = 10;
+  var current = 1;
+  var previous = 1;
+  var done = 2;
+  while (done < n) {
+    final next = current + previous;
+    previous = current;
+    current = next;
+    done += 1;
+  }
+  print('Fibonacci number $n is $current.');
+  // Fibonacci number 10 is 55.
+
+  /// Challenge 5: How many times?
+  ///
+  /// In the following for loop, what will be the value of sum, and how many
+  /// iterations will happen?
+  ///
+  /// ```
+  /// var sum = 0;
+  /// for (var i = 0; i <= 5; i++) {
+  ///   sum += i;
+  /// }
+  /// ```
+
+  var sum = 0;
+  var count = 0;
+  for (var i = 0; i <= 5; i++) {
+    sum += i;
+    count++;
+  }
+  print('The value of the sum is $sum after $count iterations.');
+  // The value of the sum is 15 after 6 iterations.
+
+  /// Challenge 6: The final countdown
+  ///
+  /// Print a countdown from 10 to 0.
+
+  for (var i = 10; i >= 0; i--) {
+    print(i);
+  }
+
+  /// Challenge 7: Print a sequence
+  ///
+  /// Print the sequence 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0.
+
+  print('0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0');
+  // Just kidding. :]
+  // Here's one of several other ways to do it:
+  for (var i = 0; i <= 10; i++) {
+    print(i / 10);
+  }
+}
---
+++ 04-control-flow/projects/final/final.dart
@@ -0,0 +1,255 @@
+// Copyright (c) 2020 Razeware LLC
+// For full license & permission details, see LICENSE.
+
+import 'dart:math';
+
+void main() {
+  /// Comparison operators
+
+  // const bool yes = true;
+  // const bool no = false;
+  const yes = true;
+  const no = false;
+
+  /// Boolean operators
+
+  const doesOneEqualTwo = (1 == 2);
+  print('doesOneEqualTwo: $doesOneEqualTwo');
+
+  const doesOneNotEqualTwo = (1 != 2);
+  print('doesOneNotEqualTwo: $doesOneNotEqualTwo');
+
+  const alsoTrue = !(1 == 2);
+  print('alsoTrue: $alsoTrue');
+
+  const isOneGreaterThanTwo = (1 > 2);
+  const isOneLessThanTwo = (1 < 2);
+  print('isOneGreaterThanTwo: $isOneGreaterThanTwo');
+  print('isOneLessThanTwo: $isOneLessThanTwo');
+
+  /// Boolean logic
+
+  const and = true && true;
+  print('and: $and');
+
+  const or = true || false;
+  print('or: $or');
+
+  const andTrue = 1 < 2 && 4 > 3;
+  const andFalse = 1 < 2 && 3 > 4;
+  const orTrue = 1 < 2 || 3 > 4;
+  const orFalse = 1 == 2 || 3 == 4;
+  print('andTrue: $andTrue');
+  print('andFalse: $andFalse');
+  print('orTrue: $orTrue');
+  print('orFalse: $orFalse');
+
+  const andOr = (1 < 2 && 3 > 4) || 1 < 4;
+  print('andOr: $andOr');
+
+  /// String equality
+
+  const guess = 'dog';
+  const dogEqualsCat = guess == 'cat';
+  print('dogEqualsCat: $dogEqualsCat');
+
+  /// The if statement
+
+  if (2 > 1) {
+    print('Yes, 2 is greater than 1.');
+  }
+
+  const animal = 'Fox';
+  if (animal == 'Cat' || animal == 'Dog') {
+    print('Animal is a house pet.');
+  } else {
+    print('Animal is not a house pet.');
+  }
+
+  const trafficLight = 'yellow';
+  var command = '';
+  if (trafficLight == 'red') {
+    command = 'Stop';
+  } else if (trafficLight == 'yellow') {
+    command = 'Slow down';
+  } else if (trafficLight == 'green') {
+    command = 'Go';
+  } else {
+    command = 'INVALID COLOR!';
+  }
+  print(command);
+
+  /// Short circuiting
+
+  const name = 'Vicki Wenderlich';
+  if (1 > 2 && name == 'Ray Wenderlich') {
+    // ...
+  }
+
+  if (1 < 2 || name == 'Ray Wenderlich') {
+    // ...
+  }
+
+  /// Encapsulating variables
+
+  // const isHungry = true;
+  // var decision = 'Skip lunch';
+  // if (isHungry) {
+  //   decision = 'Eat lunch.';
+  // }
+  // print(decision);
+
+  const isHungry = true;
+  if (isHungry) {
+    var decision = 'Eat lunch.';
+  }
+  // print(decision); // error
+
+  /// The ternary conditional operator
+
+  const a = 5;
+  const b = 10;
+
+  int min;
+  if (a < b) {
+    min = a;
+  } else {
+    min = b;
+  }
+  print('min: $min');
+
+  min = (a < b) ? a : b;
+  print('min: $min');
+
+  /// While loops
+
+  // Infinite loop
+  // while (true) { }
+
+  var sum = 1;
+  print('sum: $sum');
+  while (sum < 10) {
+    sum += 4;
+    print('sum: $sum');
+  }
+
+  /// Do-while loops
+
+  sum = 1;
+  print('sum: $sum');
+  do {
+    sum += 4;
+    print('sum: $sum');
+  } while (sum < 10);
+
+  /// Breaking out of a loop
+
+  sum = 1;
+  print('sum: $sum');
+  while (true) {
+    sum += 4;
+    print('sum: $sum');
+    if (sum > 10) {
+      break;
+    }
+  }
+
+  /// A random interlude
+
+  final random = Random();
+  while (random.nextInt(6) + 1 != 6) {
+    print('Not a six!');
+  }
+
+  /// For loops
+
+  for (var i = 0; i < 5; i++) {
+    print(i);
+  }
+
+  for (var i = 0; i < 5; i++) {
+    if (i == 2) {
+      continue;
+    }
+    print(i);
+  }
+
+  /// For-in loops
+
+  const myString = 'I ❤ Dart';
+  for (var codePoint in myString.runes) {
+    print(String.fromCharCode(codePoint));
+  }
+
+  /// For-each loops
+
+  const myNumbers = [1, 2, 3];
+  myNumbers.forEach((number) => print(number));
+
+  myNumbers.forEach((number) {
+    print(number);
+  });
+
+  /// Switch statements
+
+  const number = 10;
+  switch (number) {
+    case 0:
+      print('Zero');
+      break;
+    default:
+      print('Non-zero');
+  }
+
+  if (number == 0) {
+    print('Zero');
+  } else {
+    print('Non-zero');
+  }
+
+  const weather = 'cloudy';
+  switch (weather) {
+    case 'sunny':
+      print('Put on sunscreen.');
+      break;
+    case 'snowy':
+      print('Get your skis.');
+      break;
+    case 'cloudy':
+    case 'rainy':
+      print('Bring an umbrella.');
+      break;
+    default:
+      print("I'm not familiar with that weather.");
+  }
+
+  /// Enumerated types
+
+  // Find the enum defined below, outside of the main() function.
+  const weatherToday = Weather.cloudy;
+  switch (weatherToday) {
+    case Weather.sunny:
+      print('Put on sunscreen.');
+      break;
+    case Weather.snowy:
+      print('Get your skis.');
+      break;
+    case Weather.cloudy:
+    case Weather.rainy:
+      print('Bring an umbrella.');
+      break;
+  }
+
+  print(weatherToday);
+  final index = weatherToday.index;
+  print(index);
+}
+
+enum Weather {
+  sunny,
+  snowy,
+  cloudy,
+  rainy,
+}
+
+// enum Weather { sunny, snowy, cloudy, rainy }
