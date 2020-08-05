a2eb73f18336ed4f5dbfa9e770510242db783555
suragch <studymongolian@gmail.com>
2020-08-05 09:44:12 +0800

    progress toward first draft

---
+++ 08-classes/projects/challenge/mini-exercise.dart
@@ -0,0 +1,23 @@
+import 'dart:html';
+
+void main() {
+
+}
+
+// class Password {
+//   String value = '';
+
+//   bool isValid() {
+//     return value.length > 8;
+//   }
+
+//   @override
+//   String toString() {
+//     return value;
+//   }
+// }
+
+class Password {
+  const Password(this.value);
+  final String value;
+}
