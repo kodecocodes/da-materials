63223dbb2078fbb367bd79b5bae0a25930156c79
suragch <suragch@suragch.dev>
2022-03-26 18:52:10 +0800

    Chapter 5 updates

--- 05-functions/projects/challenge/bin/challenge.dart
+++ 05-functions/projects/challenge/bin/challenge.dart
@@ -1,54 +1,117 @@
-// Copyright (c) 2021 Razeware LLC
+// Copyright (c) 2022 Razeware LLC
 // For full license & permission details, see LICENSE.
 
 import 'dart:math';
 
 void main(List<String> arguments) {
+  functionBasicsExercise1();
+  functionBasicsExercise2();
+  functionBasicsExercise3();
+
   challenge1();
   challenge2();
-  challenge3();
 }
 
-/// Challenge 1: Prime time
+/// Function basics: Exercise 1
+///
+/// Write a function named `youAreWonderful` with a string parameter
+/// called `name`. It returns a string using `name`, something like
+/// "You're wonderful, Bob."
+void functionBasicsExercise1() {
+  String youAreWonderful(String name) {
+    return "You're wonderful, $name.";
+  }
+
+  print(youAreWonderful('Bob'));
+}
+
+/// Function basics: Exercise 2
+///
+/// Add another `int` parameter called `numberPeople` so that the
+/// function returns something like "You're wonderful, Bob. 10 people
+/// think so."
+void functionBasicsExercise2() {
+  String youAreWonderful(String name, int numberPeople) {
+    return "You're wonderful, $name. $numberPeople people think so.";
+  }
+
+  print(youAreWonderful('Bob', 10));
+}
+
+/// Function basics: Exercise 3
+///
+///  Make both inputs named parameters. Make `name` required and
+/// `numberPeople` have a default of `30`.
+void functionBasicsExercise3() {
+  String youAreWonderful({
+    required String name,
+    int numberPeople = 30,
+  }) {
+    return "You're wonderful, $name. $numberPeople people think so.";
+  }
+
+  print(youAreWonderful(name: 'Mary'));
+}
+
+/// Challenge 1: Circular Area
+///
+/// Write a function that tells you the area of a circle based on some input
+/// radius. If you recall from geometry class, you can find the area of a
+/// circle by multiplying pi times the radius squared.
+void challenge1() {
+  // pi is in the dart:math library.
+  double areaCircle(double radius) {
+    return pi * radius * radius;
+  }
+
+  final result = areaCircle(3);
+  print(result);
+}
+
+/// Challenge 2: Prime time
 ///
 /// Write a function that checks if a number is prime.
 ///
-/// Here is some help if you want a few hints before you look at the answer:
-///
-/// First, write a function like this:
+/// First, write a function with the following signature to determine if one
+/// number is divisible by another:
 ///
 /// ```
 /// bool isNumberDivisible(int number, int divisor)
 /// ```
 ///
-/// to determine if one number is divisible by another.
-/// Hint: Use the modulo operator %.
+/// The modulo operator `%` will help with that.
 ///
-/// Then, write the function
+/// Then, write the function that returns `true` if prime and `false` otherwise:
 ///
 /// ```
 /// bool isPrime(int number)
 /// ```
 ///
-/// that returns true if prime and false otherwise. A number is prime if
-/// it's only divisible by 1 and itself. Loop through the numbers from 1
-/// to the number and find the number's divisors. If it has any divisors
-/// other than 1 and itself, it's not prime.
+/// A number is prime if it's only divisible by 1 and itself. Loop through the
+/// numbers from 1 to the number and find the number's divisors. If it has any
+/// divisors other than 1 and itself, it's not prime.
 ///
 /// Check the following cases:
+///
+/// ```dart
 /// isPrime(6); // false
 /// isPrime(13); // true
 /// isPrime(8893); // true
+/// ```
 ///
-/// Hint 1: Numbers less than zero are not considered prime.
-/// Hint 2: Use a for loop to look for divisors. You can start at 2
-///         and if you end before the number, return false.
-/// Hint 3: If you're clever, you can loop from 2 until you reach
-///         the square root of the number. Add the following import
-///         to the top of the file to access the sqrt function:
+/// Here are a few more hints:
 ///
+/// - Numbers less than zero are not considered prime.
+/// - Use a `for` loop to look for divisors. You can start at 2 and if you
+/// end before the number, return false.
+/// - If you're clever, you can loop from 2 until you reach the square root
+/// of the number. Add the following import to the top of the file to access
+/// the `sqrt` function:
+///
+/// ```dart
 /// import 'dart:math';
-void challenge1() {
+/// ```
+void challenge2() {
   bool isNumberDivisible(int number, int divisor) {
     return number % divisor == 0;
   }
@@ -66,54 +129,4 @@ void challenge1() {
   print(isPrime(6));
   print(isPrime(13));
   print(isPrime(8893));
-}
-
-/// Challenge 2: Can you repeat that?
-///
-/// Write a function named `repeatTask` that looks like this:
-///
-/// ```
-/// int repeatTask(int times, int input, Function task)
-/// ```
-///
-/// It repeats a given `task` on `input` for `times` number of times.
-///
-/// Pass an anonymous function to `repeatTask` to square the input
-/// of 2 four times. Confirm that you get the result 65536, since
-/// 2 squared is 4,
-/// 4 squared is 16,
-/// 16 squared is 256, and
-/// 256 squared is 65536.
-void challenge2() {
-  int repeatTask(int times, int input, Function task) {
-    int result = task(input);
-    for (var i = 1; i < times; i++) {
-      result = task(result);
-    }
-    return result;
-  }
-
-  final result = repeatTask(4, 2, (num input) {
-    return input * input;
-  });
-
-  print(result);
-}
-
-/// Challenge 3: Darts and arrows
-///
-/// Update Challenge 2 to use arrow syntax.
-void challenge3() {
-  int repeatTask(int times, int input, Function task) {
-    int result = task(input);
-    for (var i = 1; i < times; i++) {
-      result = task(result);
-    }
-    return result;
-  }
-
-  // The anonymous function now uses arrow syntax.
-  final result = repeatTask(4, 2, (num input) => input * input);
-
-  print(result);
 }
