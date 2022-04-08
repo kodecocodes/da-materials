866e58e9d186a0e8f658c68b71bbd888055bc4bf
suragch <suragch@suragch.dev>
2022-04-08 13:58:35 +0800

    remove chapter 4 Strings content

--- 03-types-and-operations/projects/challenge/bin/challenge.dart
+++ 03-types-and-operations/projects/challenge/bin/challenge.dart
@@ -1,12 +1,8 @@
 // Copyright (c) 2022 Razeware LLC
 // For full license & permission details, see LICENSE.
 
-// If you get the error "Target of URI doesn't exist", make sure you have
-// run `dart pub get`. Then restart VS Code.
-import 'package:characters/characters.dart';
-
 void main() {
-  /// Data types in Dart: Mini-exercise 1
+  /// Exercise 1
   ///
   /// Create a constant called `age1` and set it equal to `42`. Create another
   /// constant called `age2` and set it equal to `21`. Check by hovering over
@@ -16,7 +12,7 @@ void main() {
   const age1 = 42;
   const age2 = 21;
 
-  /// Data types in Dart: Mini-exercise 2
+  /// Exercise 2
   ///
   /// Create a constant called `averageAge` and set it equal to the average of
   /// `age1` and `age2` using the operation `(age1 + age2) / 2`. Hover over
@@ -28,33 +24,6 @@ void main() {
   // `averageAge` is a `double` because any time you divide in Dart, the
   // result is a `double`. Division can result in non-integer answers, so
   // making the result an `int` could cause a loss of precision.
-
-  /// Strings: Mini-exercise 1
-  ///
-  /// Create a string constant called `firstName` and initialize it to your
-  /// first name. Also create a string constant called `lastName` and
-  /// initialize it to your last name.
-
-  const firstName = 'Ray';
-  const lastName = 'Wenderlich';
-
-  /// Strings: Mini-exercise 2
-  ///
-  /// Create a string constant called `fullName` by adding the `firstName` and
-  /// `lastName` constants together, separated by a space.
-
-  const fullName = firstName + ' ' + lastName;
-  print(fullName);
-
-  /// Strings: Mini-exercise 3
-  ///
-  /// Using interpolation, create a string constant called `myDetails` that
-  /// uses the `fullName` constant to create a string introducing yourself.
-  /// For example, Ray Wenderlich's string would read: "Hello, my name is Ray
-  /// Wenderlich."
-
-  const myDetails = 'Hello, my name is $fullName.';
-  print(myDetails);
 
   /// Challenge 1: Teacher's grading
   ///
@@ -70,65 +39,7 @@ void main() {
   final integerGrade = rawGrade.toInt();
   print(integerGrade);
 
-  /// Challenge 2: Same same, but different
-  ///
-  /// This string has two flags that look the same. But they aren't! One of
-  /// them is the flag of Chad and the other is the flag of Romania.
-  ///
-  /// ```
-  /// const twoCountries = '🇹🇩🇷🇴';
-  /// ```
-  ///
-  /// Which is which?
-  ///
-  /// **Hint**: Romania's regional indicator sequence is `RO`, and `R` is
-  /// `127479` in decimal. Chad, which is _Tishād_ in Arabic and _Tchad_ in
-  /// French, has a regional indicator sequence of `TD`. Sequence letter `T`
-  /// is `127481` in decimal.
-
-  const twoCountries = '🇹🇩🇷🇴';
-  print(twoCountries.runes);
-  // (127481, 127465, 127479, 127476)
-  // 127481 is T (127465 is D) so the first flag is Chad.
-  // 127479 is R (127476 is O) so the second flag is Romania.
-
-  /// Challenge 3: How many?
-  ///
-  /// Given the following string:
-  ///
-  /// ```
-  /// const vote = 'Thumbs up! 👍🏿';
-  /// ```
-  ///
-  /// How many UTF-16 code units are there?
-  /// How many Unicode code points are there?
-  /// How many Unicode grapheme clusters are there?
-
-  const vote = 'Thumbs up! 👍🏿';
-  print(vote.codeUnits.length); // 15
-  // There are 15 code units.
-  print(vote.runes.length); // 13
-  // There are 13 code points.
-  print(vote.characters.length); // 12
-  // There are 12 grapheme clusters.
-  // See pubspec.yaml for the characters package dependency.
-  // See the top of this file for the characters package import.
-
-  /// Challenge 4: Find the error
-  ///
-  /// What is wrong with the following code?
-  ///
-  /// ```
-  /// const name = 'Ray';
-  /// name += ' Wenderlich';
-  /// ```
-
-  var name = 'Ray';
-  name += ' Wenderlich';
-  print(name);
-  // `const` variables can't be changed, so you need to use `var`.
-
-  /// Challenge 5: What type?
+  /// Challenge 2: What type?
   ///
   /// What is the type of `value`?
   ///
@@ -141,20 +52,4 @@ void main() {
   // `value` is a `double`.
   // Division always produces a double in Dart.
   // You can see the type by hovering your mouse pointer over `value`.
-
-  /// Challenge 6: In summary
-  ///
-  /// What is the value of the constant named `summary`?
-  ///
-  /// ```
-  /// const number = 10;
-  /// const multiplier = 5;
-  /// final summary = '$number \u00D7 $multiplier = ${number * multiplier}';
-  /// ```
-
-  const number = 10;
-  const multiplier = 5;
-  final summary = '$number \u00D7 $multiplier = ${number * multiplier}';
-  print(summary);
-  // 10 × 5 = 50
 }
--- 03-types-and-operations/projects/final/bin/final.dart
+++ 03-types-and-operations/projects/final/bin/final.dart
@@ -1,10 +1,6 @@
 // Copyright (c) 2021 Razeware LLC
 // For full license & permission details, see LICENSE.
 
-// If you get the error "Target of URI doesn't exist", make sure you have
-// run `dart pub get`. Then restart VS Code.
-import 'package:characters/characters.dart';
-
 void main() {
   /// Type inference
 
@@ -48,90 +44,6 @@ void main() {
   print(someInt.isEven);
   // final someDouble = someNumber as double;
   final someDouble = someNumber.toDouble();
-
-  /// Strings and characters
-
-  print('Hello, Dart!');
-  var greeting = 'Hello, Dart!';
-  greeting = 'Hello, Flutter!';
-  print(greeting);
-
-  /// Getting characters
-
-  const letter = 'a';
-
-  var salutation = 'Hello!';
-  print(salutation.codeUnits);
-
-  const dart = '🎯';
-  print(dart.codeUnits);
-  print(dart.runes);
-
-  /// Unicode grapheme clusters
-
-  const flag = '🇲🇳';
-  print(flag.runes);
-
-  const family = '👨‍👩‍👧‍👦';
-  print(family.runes);
-
-  print(family.length);
-  print(family.codeUnits.length);
-  print(family.runes.length);
-  print(family.characters.length);
-
-  /// Single-quotes vs double-quotes
-
-  print('I like cats');
-  print("I like cats");
-  print("my cat's food");
-  print('my cat\'s food');
-
-  /// Concatenation
-
-  var message = 'Hello' + ' my name is ';
-  const name = 'Ray';
-  message += name;
-  print(message);
-
-  /// Interpolation
-
-  // const name = 'Ray';
-  const introduction = 'Hello my name is $name';
-  print(introduction);
-
-  const oneThird = 1 / 3;
-  // const sentence = 'One third is $oneThird.';
-  final sentence = 'One third is ${oneThird.toStringAsFixed(3)}.';
-  print(sentence);
-
-  /// Multi-line strings
-
-  const bigString = '''
-You can have a string 
-that contains multiple 
-lines
-by
-doing this.''';
-  print(bigString);
-
-  // const oneLine = 'This is only '
-  //   'a single '
-  //   'line '
-  //   'at runtime.';
-  const oneLine = 'This is only ' + 'a single ' + 'line ' + 'at runtime.';
-  print(oneLine);
-
-  const twoLines = 'This is\ntwo lines.';
-  print(twoLines);
-
-  const rawString = r'My name \n is $name.';
-  print(rawString);
-
-  /// Inserting characters from their codes
-
-  print('I \u2764 Dart\u0021');
-  print('I love \u{1F3AF}');
 
   /// Object and dynamic types
 
