ea1cc61cf1eb3801066c9bb7e7448fea4249c45f
suragch <suragch@suragch.dev>
2022-04-08 13:53:35 +0800

    Remove chapter 3 content

--- 04-strings/projects/challenge/bin/challenge.dart
+++ 04-strings/projects/challenge/bin/challenge.dart
@@ -6,30 +6,7 @@
 import 'package:characters/characters.dart';
 
 void main() {
-  /// Data types in Dart: Mini-exercise 1
-  ///
-  /// Create a constant called `age1` and set it equal to `42`. Create another
-  /// constant called `age2` and set it equal to `21`. Check by hovering over
-  /// the variable names that the type for both has been inferred correctly as
-  /// `int`.
-
-  const age1 = 42;
-  const age2 = 21;
-
-  /// Data types in Dart: Mini-exercise 2
-  ///
-  /// Create a constant called `averageAge` and set it equal to the average of
-  /// `age1` and `age2` using the operation `(age1 + age2) / 2`. Hover over
-  /// `averageAge` to check the type. Then check the result of `averageAge`. Why
-  /// is it a `double` if the components are all `int`?
-
-  const averageAge = (age1 + age2) / 2;
-  print(averageAge);
-  // `averageAge` is a `double` because any time you divide in Dart, the
-  // result is a `double`. Division can result in non-integer answers, so
-  // making the result an `int` could cause a loss of precision.
-
-  /// Strings: Mini-exercise 1
+  /// Exercise 1
   ///
   /// Create a string constant called `firstName` and initialize it to your
   /// first name. Also create a string constant called `lastName` and
@@ -38,7 +15,7 @@ void main() {
   const firstName = 'Ray';
   const lastName = 'Wenderlich';
 
-  /// Strings: Mini-exercise 2
+  /// Exercise 2
   ///
   /// Create a string constant called `fullName` by adding the `firstName` and
   /// `lastName` constants together, separated by a space.
@@ -46,7 +23,7 @@ void main() {
   const fullName = firstName + ' ' + lastName;
   print(fullName);
 
-  /// Strings: Mini-exercise 3
+  /// Exercise 3
   ///
   /// Using interpolation, create a string constant called `myDetails` that
   /// uses the `fullName` constant to create a string introducing yourself.
@@ -56,21 +33,7 @@ void main() {
   const myDetails = 'Hello, my name is $fullName.';
   print(myDetails);
 
-  /// Challenge 1: Teacher's grading
-  ///
-  /// You're a teacher and in your class attendance is worth 20% of the grade,
-  /// the homework is worth 30% and the exam is worth 50%. Your student got 90
-  /// points for her attendance, 80 points for her homework and 94 points on
-  /// her exam. Calculate her grade as an integer percentage rounded down.
-
-  const attendance = 90;
-  const homework = 80;
-  const exam = 94;
-  const rawGrade = (0.2 * attendance) + (0.3 * homework) + (0.5 * exam);
-  final integerGrade = rawGrade.toInt();
-  print(integerGrade);
-
-  /// Challenge 2: Same same, but different
+  /// Challenge 1: Same same, but different
   ///
   /// This string has two flags that look the same. But they aren't! One of
   /// them is the flag of Chad and the other is the flag of Romania.
@@ -92,7 +55,7 @@ void main() {
   // 127481 is T (127465 is D) so the first flag is Chad.
   // 127479 is R (127476 is O) so the second flag is Romania.
 
-  /// Challenge 3: How many?
+  /// Challenge 2: How many?
   ///
   /// Given the following string:
   ///
@@ -114,7 +77,7 @@ void main() {
   // See pubspec.yaml for the characters package dependency.
   // See the top of this file for the characters package import.
 
-  /// Challenge 4: Find the error
+  /// Challenge 3: Find the error
   ///
   /// What is wrong with the following code?
   ///
@@ -128,21 +91,7 @@ void main() {
   print(name);
   // `const` variables can't be changed, so you need to use `var`.
 
-  /// Challenge 5: What type?
-  ///
-  /// What is the type of `value`?
-  ///
-  /// ```
-  /// const value = 10 / 2;
-  /// ```
-
-  const value = 10 / 2;
-  print(value); // 5.0
-  // `value` is a `double`.
-  // Division always produces a double in Dart.
-  // You can see the type by hovering your mouse pointer over `value`.
-
-  /// Challenge 6: In summary
+  /// Challenge 4: In summary
   ///
   /// What is the value of the constant named `summary`?
   ///
--- 04-strings/projects/final/bin/final.dart
+++ 04-strings/projects/final/bin/final.dart
@@ -6,49 +6,6 @@
 import 'package:characters/characters.dart';
 
 void main() {
-  /// Type inference
-
-  // int myInteger = 10;
-  // double myDouble = 3.14;
-  // const int myInteger = 10;
-  // const double myDouble = 3.14;
-  // final int myInteger = 10;
-  // final double myDouble = 3.14;
-  const myInteger = 10;
-  const myDouble = 3.14;
-
-  num myNumber = 3.14;
-  print(myNumber is double);
-  print(myNumber is int);
-  print(myNumber.runtimeType);
-
-  /// Type conversion
-
-  var integer = 100;
-  var decimal = 12.5;
-  // integer = decimal;
-  integer = decimal.toInt();
-  print(integer);
-
-  const hourlyRate = 19.5;
-  const hoursWorked = 10;
-  // const totalCost = hourlyRate * hoursWorked;
-  // const totalCost = (hourlyRate * hoursWorked).toInt();
-  final totalCost = (hourlyRate * hoursWorked).toInt();
-  print(totalCost);
-
-  // const wantADouble = 3;
-  // final actuallyDouble = 3.toDouble();
-  const double actuallyDouble = 3;
-  const wantADouble = 3.0;
-
-  num someNumber = 3;
-  // print(someNumber.isEven);
-  final someInt = someNumber as int;
-  print(someInt.isEven);
-  // final someDouble = someNumber as double;
-  final someDouble = someNumber.toDouble();
-
   /// Strings and characters
 
   print('Hello, Dart!');
@@ -132,19 +89,4 @@ doing this.''';
 
   print('I \u2764 Dart\u0021');
   print('I love \u{1F3AF}');
-
-  /// Object and dynamic types
-
-  // var myVariable = 42;
-  // myVariable = 'hello'; // compile-time error
-
-  // dynamic myVariable = 42;
-  // myVariable = 'hello'; // OK
-
-  // var myVariable;       // defaults to dynamic
-  // myVariable = 42;      // OK
-  // myVariable = 'hello'; // OK
-
-  Object? myVariable = 42;
-  myVariable = 'hello'; // OK
 }
